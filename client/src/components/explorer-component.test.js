e02c7772bccef3df646be485fdb229b5dc676eb5
Bigghead <shavenchew@yahoo.com>
2018-06-05 15:41:11 -0700

    test input changes + form submits

--- client/src/components/explorer-component.test.js
+++ client/src/components/explorer-component.test.js
@@ -6,6 +6,12 @@ configure({ adapter: new Adapter() });
 
 import ExplorerComponent from './explorer-component';
 
+const newValues = {
+    email   : 'hi@hi.com',
+    fullName: 'Hello Hello',
+    phone   : '111-1111'
+};
+
 const config = {
     title: 'Add new user',
     url: 'https://jsonplaceholder.typicode.com/posts',
@@ -40,13 +46,48 @@ beforeEach(() => {
     );
 });
 
+
 test( 'should render local state dynamically' , () => {
     
     const localState = Explorer.state();
     const { body: { email, phone } } = localState;
     const fullName = localState.body.full-name;
-    expect(email).not.toBeNull();
-    expect(phone).not.toBeNull();
-    expect(fullName).not.toBeNull();
+    expect(email).not.toBeUndefined();
+    expect(phone).not.toBeUndefined();
+    expect(fullName).not.toBeUndefined();
+
+    expect(localState.body.name).toBeUndefined();
     
-})
+});
+
+
+test('should change state on input changes', () => {
+
+    Object.keys(newValues)
+          .forEach( ( val, i ) => {
+               let value = newValues[val];
+               Explorer.find("input")
+                       .at(i)
+                       .simulate("change", {
+                         target: {
+                           value
+                         }
+               } );
+          } )
+
+    expect(Explorer.state().body.email).toBe(newValues.email);
+    expect(Explorer.state().body['full-name']).toBe(newValues.fullName);
+    expect(Explorer.state().body.phone).toBe(newValues.phone);
+
+});
+
+
+test("should not submit invalid form submission", () => {
+    const submitForm = jest.spyOn(Explorer.instance(), 'submitForm')
+
+    Explorer.find('form').simulate('submit', { preventDefault() {} });
+    expect(submitForm).toHaveBeenCalledTimes(0);
+});
+
+
+
